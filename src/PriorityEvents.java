83d0ae8ab6e74b6cea08168534ca16ab0890e2e1
Erik Larson <erikmlarson5@gmail.com>
2025-04-18 13:52:28 -0500

    file headers

--- src/PriorityEvents.java
+++ src/PriorityEvents.java
@@ -1,2 +1,20 @@
+//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
+//
+// Title:    P10 Priority Events
+// Course:   CS 300 Spring 2025
+//
+// Author:   Erik Larson
+// Email:    emlarson23@wisc.edu
+// Lecturer: Hobbes LeGault
+//
+//////////////////////// ASSISTANCE/HELP CITATIONS ////////////////////////////
+//
+// Persons:         N/A (did not use or reference Hobbes' code in lecture)
+// Online Sources:  https://www.geeksforgeeks.org/priority-queue-using-binary-heap/ used the
+// diagrams in this website to visualize insert and remove (addEvent and completeEvent). Did not
+// use any code within the G4G website.
+//
+///////////////////////////////////////////////////////////////////////////////
+
 public class PriorityEvents {
 }
--- src/PriorityEventsTester.java
+++ src/PriorityEventsTester.java
@@ -1,4 +1,20 @@
-// TODO File header
+//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
+//
+// Title:    P10 Priority Events
+// Course:   CS 300 Spring 2025
+//
+// Author:   Erik Larson
+// Email:    emlarson23@wisc.edu
+// Lecturer: Hobbes LeGault
+//
+//////////////////////// ASSISTANCE/HELP CITATIONS ////////////////////////////
+//
+// Persons:         N/A (did not use or reference Hobbes' code in lecture)
+// Online Sources:  https://www.geeksforgeeks.org/priority-queue-using-binary-heap/ used the
+// diagrams in this website to visualize insert and remove (addEvent and completeEvent). Did not
+// use any code within the G4G website.
+//
+///////////////////////////////////////////////////////////////////////////////
 
 /**
  * Tester class for the CS300 P10 Priority Events project. You may add tester methods to this class
